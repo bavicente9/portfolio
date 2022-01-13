6507333550f63ce799a3e0a61948ac6713d0fd43
bavicente9 <bavicente98@gmail.com>
2022-01-13 18:15:51 -0500

    presentation updated

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -5,7 +5,7 @@ export const Presentation = () => {
     return (
         <section aria-label='Presentation' id='presentation' className='presentation'>
             <div className='presentation__container'>
-                <h1>Hello! i'm Bryan Vicente, web developer.</h1>
+                <h1>Hi! i'm Bryan Vicente, web developer.</h1>
                 <p>I am passionate about technology,improve my skills , get new knowledge continuously, and build  the bests websites with good practics and a great accessibility.</p>
                 <div className='presentation__buttons'>
                     <a rel='external' className='button button-presentation' href='https://github.com/bavicente9/portfolio/blob/master/public/resume/resume-Bryan_Vicente.pdf' >Resume</a>
